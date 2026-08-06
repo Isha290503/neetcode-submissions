399b219537f0fab5188b76fa4a67e0f5b7493b90
Isha <isha.id2905@gmail.com>
2026-08-07 03:20:56 +0530

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n=s.length();
+        int count=0;
+        boolean[][] dp=new boolean[n][n];
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s.charAt(i)==s.charAt(j)){
+                    if(j-i+1<=3) dp[i][j]=true;
+                    else dp[i][j]=dp[i+1][j-1];
+                }
+                if(dp[i][j]) count++;
+            }
+        }
+        return count;
+    }
+}
