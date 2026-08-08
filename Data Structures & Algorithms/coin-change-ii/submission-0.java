8326342938f487fff7d36edab728d6934fde108a
Isha <isha.id2905@gmail.com>
2026-08-08 17:57:17 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp=new int[amount+1];
+        dp[0]=1;
+        for(int coin:coins){
+            for(int i=coin;i<=amount;i++){
+                if(i-coin>=0){
+                    if(dp[i-coin]!=0)
+                        dp[i]+=dp[i-coin];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
