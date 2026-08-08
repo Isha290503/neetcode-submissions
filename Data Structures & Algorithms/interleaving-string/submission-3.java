9ab62863522e69f7fa90c29a65955ed133cf066c
Isha <isha.id2905@gmail.com>
2026-08-09 00:39:58 +0530

    Add: interleaving-string - submission-3

---
+++ Data Structures & Algorithms/interleaving-string/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m=s1.length();
+        int n=s2.length();
+        if(m+n!=s3.length()) return false;
+        int[][]dp=new int[m+1][n+1];
+        for (int i = 0; i <= m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return recurse(s1,s2,s3,0,0,dp);
+    }
+    public boolean recurse(String s1, String s2, String s3,int i,int j,int[][] dp){
+        if(i==s1.length() && j==s2.length()) return true;
+        if(dp[i][j]!=-1) return dp[i][j] == 1;
+        int k=i+j;
+        if(i<s1.length() && s1.charAt(i)==s3.charAt(k)){
+            if(recurse(s1,s2,s3,i+1,j,dp)){
+                dp[i][j]=1;
+                return true;
+            }
+        }
+        if(j<s2.length() && s2.charAt(j)==s3.charAt(k)){
+            if(recurse(s1,s2,s3,i,j+1,dp)){
+                dp[i][j]=1;
+                return true;
+            }
+        }
+        dp[i][j]=0;
+        return false;
+    }
+}
