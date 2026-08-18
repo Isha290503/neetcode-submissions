f7a0ad7bc70dffce445b0ee55eb2de2cabd9ad2d
Isha <isha.id2905@gmail.com>
2026-08-19 03:41:14 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        Stack<int[]> st=new Stack<>();
+        int[] res=new int[temp.length];
+        for(int i=0;i<temp.length;i++){
+            while(!st.isEmpty() && st.peek()[0]<temp[i]){
+                int[] cur=st.pop();
+                res[cur[1]]=i-cur[1];
+            }
+            st.push(new int[]{temp[i],i});
+        }
+        return res;
+    }
+}
