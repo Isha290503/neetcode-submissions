a8c67c4b53c70758c5dfb65a23825bc2e350bad2
Isha <isha.id2905@gmail.com>
2026-08-23 22:36:32 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    List<List<Integer>> l=new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        recurse(0,nums,new ArrayList<>());
+        return l;
+    }
+    public void recurse(int i,int[] nums,List<Integer> temp){
+        if(i==nums.length){
+            l.add(new ArrayList<>(temp));
+            return;
+        }
+        temp.add(nums[i]);
+        recurse(i+1,nums,temp);
+        temp.remove(temp.size()-1);
+        recurse(i+1,nums,temp);
+    }
+}
